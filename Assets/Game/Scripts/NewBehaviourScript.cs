80e5a4189fb0987822f55b49cc25d5af5ca59cf7
IT <igor-terentev@mail.ru>
2025-10-25 12:19:53 +0300

    Assets added

--- Assets/Game/Scripts/NewBehaviourScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
-{
-    private void Start()
-    {
-        Debug.Log($"ДЛоукрпывздалмтхкщпо упхзво");
-    }
-}
--- Assets/Game/Scripts/NewBehaviourScript1.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class NewBehaviourScript1 : MonoBehaviour
-{
-    private void Start()
-    {
-        Debug.Log($"develop");
-    }
-}
